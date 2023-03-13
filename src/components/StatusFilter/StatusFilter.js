d9d7038933f5d42dbe5eefd975b60e0d1d683903
Petro Psekho <psekho.pp@gmail.com>
2023-03-13 15:13:31 +0200

    adds stor subscription

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,20 @@
-import { Button } from "components/Button/Button";
-import css from "./StatusFilter.module.css";
+// Імпортуємо хук
+import { useSelector } from 'react-redux';
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from 'redux/constants';
+import { getStatusFilter } from 'redux/selectors';
+
+import { Button } from 'components/Button/Button';
+import css from './StatusFilter.module.css';
 
 export const StatusFilter = () => {
+  // Отримуємо значення фільтра із стану Redux
+  const filter = useSelector(state => getStatusFilter);
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
     </div>
   );
 };
